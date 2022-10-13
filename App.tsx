b3772d89950a894a7f1fa20dd273706bec344004
pagecow <pagecow@gmail.com>
2022-10-13 11:35:52 -0500

    update

--- App.tsx
+++ App.tsx
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Welcome from './Components/Welcome';
+import { Pet } from './Components/Pet';
+import { PetQualities } from './Components/PetQualities';
+
 export default function App() {
+  const petName = {
+    firstName: "Roger",
+    lastName: "Porticous"
+  }
+
+  const qualities = [
+    {
+      qualOne: 'a lizard',
+      qualTwo: 'enormous',
+      qualThree: 'scary as a mother who caught her child eating her secret stash of chocolate',
+      age: 17
+    },
+    {
+      qualOne: 'green',
+      qualTwo: 'sometimes yellow',
+      qualThree: 'potentially purple',
+      age: 17
+    }
+  ]
+
+  const constMessage = "Are these things actually true? No one really knows.";
+
+  const [finalMessage, setFinalMessage] = useState("But I'm assuming it's true because I think all lizards change colors. But I'm too lazy to do a Google search to find out if this is true.")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Welcome name="Jack" age={37} gender={true} />
+      <Pet petName={petName} type="Komodo Dragon" />
+      <PetQualities qualities={qualities} />
+      <Text>{constMessage}</Text>
+      <Text>{finalMessage}</Text>
     </View>
   );
 }
@@ -16,5 +47,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 30
   },
 });
---
+++ Components/Welcome.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+type WelcomeProps = {
+    name: string,
+    age: number,
+    gender: boolean
+}
+
+export default function Welcome(props: WelcomeProps) {
+    return (
+        <View>
+            <Text>Hello {props.name}!</Text>
+            <Text>You are {props.age}.</Text>
+            <Text>You are a {props.gender ? "man" : "woman"}.</Text>
+        </View>
+    )
+}
